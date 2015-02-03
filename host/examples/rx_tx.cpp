e0536f5c50dea4b6b1b15745b70f2364bdb9e46f
Alexey Shamrin <shamrin@gmail.com>
2015-02-03 12:54:22 +0000

    add ring buffer (of buffers)

--- host/examples/rx_tx.cpp
+++ host/examples/rx_tx.cpp
@@ -27,10 +27,131 @@
 #include <complex>
 #include <csignal>
 
+#include <semaphore.h>
+#include <assert.h>
+
 namespace po = boost::program_options;
 
 static bool stop_signal_called = false;
 void sig_int_handler(int){ std::cerr << "Stopping..."; stop_signal_called = true; }
+
+template <typename samp_type, size_t NBUFS>
+class Loopbacker {
+public:
+    typedef std::vector<samp_type> BufT;
+    BufT* ring_buffer[NBUFS];
+    sem_t ring_get_sem, ring_put_sem;
+    int ring_get_i, ring_put_i;
+    size_t samps_per_buff;
+
+    // http://en.wikipedia.org/wiki/Circular_buffer#Read_.2F_Write_Counts
+    int read_count, write_count;
+
+    Loopbacker(size_t samps_per_buff) : samps_per_buff(samps_per_buff) {
+        for(int i = 0; i < NBUFS; i++) {
+            ring_buffer[i] = new BufT(samps_per_buff);
+        }
+        ring_get_i = ring_put_i = 0;
+        read_count = write_count = 0;
+        sem_init(&ring_get_sem, 0, 0);
+        sem_init(&ring_put_sem, 0, NBUFS);
+    }
+    ~Loopbacker() {
+        for(int i = 0; i < NBUFS; i++) { delete ring_buffer[i]; }
+    }
+
+    inline int ring_size() { return write_count - read_count; }
+
+    BufT* ring_get() {
+        assert(ring_size() > 0);
+        int i = ring_get_i;
+        ring_get_i = (ring_get_i - 1) % NBUFS;
+        read_count++;
+        return ring_buffer[i];
+    }
+
+    BufT* ring_put() {
+        assert(ring_size() < NBUFS);
+        int i = ring_put_i;
+        ring_put_i = (ring_put_i + 1) % NBUFS;
+        write_count++;
+        return ring_buffer[i];
+    }
+
+    void send(uhd::tx_streamer::sptr tx_stream, uhd::tx_metadata_t& tx_md) {
+        sem_wait(&ring_get_sem);
+        BufT* buff = ring_get();
+
+        /*size_t num_samps = */tx_stream->send(&buff->front(), buff->size(), tx_md);
+//        std::cerr << boost::format("send %d\n") % (num_samps);
+        sem_post(&ring_put_sem);
+    }
+
+    void recv(uhd::rx_streamer::sptr rx_stream, uhd::rx_metadata_t& rx_md) {
+        sem_wait(&ring_put_sem);
+        BufT* buff = ring_put();
+
+        /*size_t num_samps = */rx_stream->recv(&buff->front(), buff->size(), rx_md, 3.0);
+//        std::cerr << boost::format("recv %d\n") % (num_samps);
+        sem_post(&ring_get_sem);
+    }
+
+    void send_loop(
+        uhd::usrp::multi_usrp::sptr usrp,
+        const uhd::stream_args_t& stream_args
+    ) {
+        uhd::tx_streamer::sptr tx_stream = usrp->get_tx_stream(stream_args);
+        uhd::tx_metadata_t tx_md;
+        while(not stop_signal_called){
+            send(tx_stream, tx_md);
+        }
+    }
+
+    void recv_loop(
+        uhd::usrp::multi_usrp::sptr usrp,
+        const uhd::stream_args_t& stream_args
+    ) {
+        uhd::rx_streamer::sptr rx_stream = usrp->get_rx_stream(stream_args);
+        uhd::rx_metadata_t rx_md;
+        bool overflow_message = true;
+
+        //setup streaming
+        uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
+        stream_cmd.num_samps = 0;
+        stream_cmd.stream_now = true;
+        stream_cmd.time_spec = uhd::time_spec_t();
+        rx_stream->issue_stream_cmd(stream_cmd);
+
+        while(not stop_signal_called){
+            recv(rx_stream, rx_md);
+
+            if (rx_md.error_code == uhd::rx_metadata_t::ERROR_CODE_TIMEOUT) {
+                std::cout << boost::format("Timeout while streaming") << std::endl;
+                break;
+            }
+            if (rx_md.error_code == uhd::rx_metadata_t::ERROR_CODE_OVERFLOW){
+                if (overflow_message) {
+                    overflow_message = false;
+                    std::cerr << boost::format(
+                        "Got an overflow indication. Please consider the following:\n"
+                        "  Your write medium must sustain a rate of %fMB/s.\n"
+                        "  Dropped samples will not be transmitted.\n"
+                        "  This message will not appear again.\n"
+                    ) % (usrp->get_rx_rate()*sizeof(samp_type)/1e6);
+                }
+                continue;
+            }
+            if (rx_md.error_code != uhd::rx_metadata_t::ERROR_CODE_NONE){
+                std::string error = str(boost::format("Receiver error: %s") % rx_md.strerror());
+                std::cerr << error << std::endl;
+                continue;
+            }
+        }
+
+        stream_cmd.stream_mode = uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS;
+        rx_stream->issue_stream_cmd(stream_cmd);
+    }
+};
 
 template<typename samp_type> void loopback(
     uhd::usrp::multi_usrp::sptr usrp,
@@ -38,55 +159,19 @@ template<typename samp_type> void loopback(
     const std::string &wire_format,
     size_t samps_per_buff
 ){
-
-    //create streamers
     uhd::stream_args_t stream_args(cpu_format, wire_format);
-    uhd::tx_streamer::sptr tx_stream = usrp->get_tx_stream(stream_args);
-    uhd::rx_streamer::sptr rx_stream = usrp->get_rx_stream(stream_args);
 
-    uhd::tx_metadata_t tx_md;
-    uhd::rx_metadata_t rx_md;
-    bool overflow_message = true;
-    std::vector<samp_type> buff(samps_per_buff);
+    #define N_BUFFERS 32
 
-    //setup streaming
-    uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
-    stream_cmd.num_samps = 0;
-    stream_cmd.stream_now = true;
-    stream_cmd.time_spec = uhd::time_spec_t();
-    rx_stream->issue_stream_cmd(stream_cmd);
+    Loopbacker<samp_type, N_BUFFERS> loopbacker(samps_per_buff);
 
-    //loopback
-    while(not stop_signal_called){
-        size_t num_samps = rx_stream->recv(&buff.front(), buff.size(), rx_md, 3.0);
+    boost::thread recv_thread(&Loopbacker<samp_type, N_BUFFERS>::recv_loop,
+                              &loopbacker, usrp, stream_args);
+    boost::thread send_thread(&Loopbacker<samp_type, N_BUFFERS>::send_loop,
+                              &loopbacker, usrp, stream_args);
 
-        if (rx_md.error_code == uhd::rx_metadata_t::ERROR_CODE_TIMEOUT) {
-            std::cout << boost::format("Timeout while streaming") << std::endl;
-            break;
-        }
-        if (rx_md.error_code == uhd::rx_metadata_t::ERROR_CODE_OVERFLOW){
-            if (overflow_message) {
-                overflow_message = false;
-                std::cerr << boost::format(
-                    "Got an overflow indication. Please consider the following:\n"
-                    "  Your write medium must sustain a rate of %fMB/s.\n"
-                    "  Dropped samples will not be transmitted.\n"
-                    "  This message will not appear again.\n"
-                ) % (usrp->get_rx_rate()*sizeof(samp_type)/1e6);
-            }
-            continue;
-        }
-        if (rx_md.error_code != uhd::rx_metadata_t::ERROR_CODE_NONE){
-            std::string error = str(boost::format("Receiver error: %s") % rx_md.strerror());
-            std::cerr << error << std::endl;
-            continue;
-        }
-
-        tx_stream->send(&buff.front(), num_samps, tx_md);
-    }
-
-    stream_cmd.stream_mode = uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS;
-    rx_stream->issue_stream_cmd(stream_cmd);
+    recv_thread.join();
+    send_thread.join();
 }
 
 int UHD_SAFE_MAIN(int argc, char *argv[]){
